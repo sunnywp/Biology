e11c90a205e3233aff590edfacbdf3009420f8b8
wangping <10196470@zte.intra>
2018-11-30 17:20:09 +0800

    ant design

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,11 @@ import { NgModule } from '@angular/core';
 import { routing } from './app.routes';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home.component';
+import { NgZorroAntdModule, NZ_I18N, zh_CN } from 'ng-zorro-antd';
+/** 配置 angular i18n **/
+import { registerLocaleData } from '@angular/common';
+import zh from '@angular/common/locales/zh';
+registerLocaleData(zh);
 
 @NgModule({
   declarations: [
@@ -11,9 +16,10 @@ import { HomeComponent } from './home.component';
   ],
   imports: [
     routing,
-    BrowserModule
+    BrowserModule,
+    NgZorroAntdModule
   ],
-  providers: [],
+  providers: [{ provide: NZ_I18N, useValue: zh_CN }],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
