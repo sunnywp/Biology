e1e6418074f34cb5b857f8d9afd7b492fb75d14e
wangping <wang.ping6@zte.com.cn>
2019-01-04 17:23:11 +0800

    fix less and add polyfills

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,19 +1,18 @@
-import { RouterModule } from '@angular/router';
-import { ModuleWithProviders }  from '@angular/core';
+import {RouterModule} from '@angular/router';
+import {ModuleWithProviders} from '@angular/core';
+import {HomeComponent} from './home.component';
 
-import { HomeComponent } from './home.component';
 
-
-export const appRoutes=[
-	{
-		path:'',
-		redirectTo:'home',
-		pathMatch:'full'
-	},
-	{
-		path:"home",
-		component:HomeComponent
-	}
+export const appRoutes = [
+  {
+    path: '',
+    redirectTo: 'home',
+    pathMatch: 'full'
+  },
+  {
+    path: 'home',
+    component: HomeComponent
+  }
 ];
 
 export const routing: ModuleWithProviders = RouterModule.forRoot(appRoutes);
--- src/app/home.component.ts
+++ src/app/home.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit} from '@angular/core';
-declare var Swiper:any;
+import {Component, OnInit} from '@angular/core';
+
+declare var Swiper: any;
 
 @Component({
   selector: 'app-home',
@@ -8,21 +9,22 @@ declare var Swiper:any;
 })
 
 export class HomeComponent implements OnInit {
-  	constructor() { 
-    }
-  	ngOnInit() {
-      var swiper = new Swiper('.swiper-container', {
-        slidesPerView: 1,
-        spaceBetween: 30,
-        loop: true,
-        pagination: {
-          el: '.swiper-pagination',
-          clickable: true,
-        },
-        navigation: {
-          nextEl: '.swiper-button-next',
-          prevEl: '.swiper-button-prev',
-        },
-      });
-  	}
+  constructor() {
+  }
+
+  ngOnInit() {
+    const swiper = new Swiper('.swiper-container', {
+      slidesPerView: 1,
+      spaceBetween: 30,
+      loop: true,
+      pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+      },
+      navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+      },
+    });
+  }
 }
