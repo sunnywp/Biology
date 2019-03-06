deba23411a8420e63bc9a9fd9bde128325bd1190
wang.ping <1169298955@qq.com>
2019-03-06 22:14:24 +0800

    add product

--- src/app/common/product-profile/productProfile.component.ts
+++ src/app/common/product-profile/productProfile.component.ts
@@ -1,4 +1,4 @@
-import {Component} from '@angular/core';
+import {Component, Input} from '@angular/core';
 
 @Component({
   selector: 'product-profile',
@@ -7,6 +7,7 @@ import {Component} from '@angular/core';
 })
 
 export class ProductProfileComponent {
-
+  @Input() imageUrl: string = '';
+  @Input() content: string = '';
 
 }
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -9,7 +9,28 @@ declare var Swiper: any;
 })
 
 export class HomeComponent implements OnInit {
-  productList:any[] = [];
+  productList:any[] = [
+    {url:'assets/images/4.jpg',
+    content:'Acura® manual手动移液器'},
+    {url:'assets/images/5.jpg',
+      content:'Acura® electro电动移液器'},
+    {url:'assets/images/6.jpg',
+      content:'Acurex™紧凑型瓶口配液器'},
+    {url:'assets/images/7.jpg',
+      content:'Calibrex™瓶口分液器'},
+    {url:'assets/images/8.jpg',
+      content:'Dosys™连续分液注射器'},
+    {url:'assets/images/9.jpg',
+      content:' Profiller™移液管控制器'},
+    {url:'assets/images/10.jpg',
+      content:'Acura®self-refill连续分液器'},
+    {url:'assets/images/11.jpg',
+      content:'Stepper™ 连续注射移液器'},
+    {url:'assets/images/12.jpg',
+      content:'Qualitix®移液器耗材'},
+    {url:'assets/images/13.jpg',
+      content:'生物培养消耗品'}
+  ];
   constructor() {
   }
 
