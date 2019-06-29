aa0857bbe1c86a64c8d19e1df55faba32d32552b
wangping <wang.ping6@zte.com.cn>
2019-06-29 18:48:39 +0800

    add router

--- server/app.js
+++ server/app.js
@@ -1,8 +1,23 @@
 const Koa = require('koa');
+const router = require('koa-router')();
 const app = new Koa();
 
-const main = ctx => {
-  ctx.response.body = 'Hello World';
-};
-app.use(main);
+// log request URL:
+app.use(async (ctx, next) => {
+  console.log(`Process ${ctx.request.method} ${ctx.request.url}...`);
+  await next();
+});
+
+// add url-route:
+router.get('/hello/:name', async (ctx, next) => {
+  var name = ctx.params.name;
+  ctx.response.body = `<h1>Hello, ${name}!</h1>`;
+});
+
+router.get('/', async (ctx, next) => {
+  ctx.response.body = '<h1>Index</h1>';
+});
+
+// add router middleware:
+app.use(router.routes());
 app.listen(3000);
